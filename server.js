8c41027d7c9c1fc245b8fe68c9c7c185119a7285
websicle <89933280+webcicle@users.noreply.github.com>
2022-05-31 21:35:06 +0200

    fixed heroku script

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.use('/api/authors', require('./routes/authorRoutes'));
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static('frontend/build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 	});
 }
 
